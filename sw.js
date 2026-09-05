df32991f375cd4f1e52ef88f369a982551fbab9a
aloredisharre2017 <aloredisharee2017@gmail.com>
2026-09-05 23:12:18 +0600

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,21 +1,15 @@
-const CACHE_NAME = 'raktodan-cache-v1';
-const urlsToCache = [
-  './',
-  './index.html',
-  './admin.html',
-  './manifest.json'
-];
+const CACHE_NAME = 'raktodan-v2';
 
-self.addEventListener('install', event => {
-  event.waitUntil(
-    caches.open(CACHE_NAME)
-      .then(cache => cache.addAll(urlsToCache))
-  );
+self.addEventListener('install', (event) => {
+    self.skipWaiting();
 });
 
-self.addEventListener('fetch', event => {
-  event.respondWith(
-    caches.match(event.request)
-      .then(response => response || fetch(event.request))
-  );
+self.addEventListener('activate', (event) => {
+    event.waitUntil(clients.claim());
+});
+
+self.addEventListener('fetch', (event) => {
+    event.respondWith(
+        fetch(event.request).catch(() => caches.match(event.request))
+    );
 });
